5279a49f8af89efe67be44d9506a696521d1aab4
Manol Sharma <manol.sharma@ManolSharmaOmaha-HQ-NE.local>
2024-05-06 11:27:02 -0500

    testing the husky formatter

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
 import Image from "next/image";
 
+
+const hello = 'hello there'
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
